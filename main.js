fb2f7afdfd14de48e70dbaf3d83b0d943e1511a4
asddsada <baipo_p@hotmail.com>
2020-11-27 16:46:34 +0700

    Add invalid then loop question

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ var total_win = 0
 const placeBet = () => {
   return new Promise((resolve, reject) => {
     rl.question('Please place your bet.\n', (bet) => {
-      if (bet.match(/^\d/)){
+      if (bet.match(/^\d/)) {
         resolve(parseInt(bet))
-      }else{
+      } else {
         reject('Invalid input')
       }
     })
@@ -28,11 +28,11 @@ const playMoreAsk = () => {
   return new Promise((resolve, reject) => {
     rl.question('Wanna play more (Yes/No)?\n', (answer) => {
       answer = answer.toLowerCase()
-      if (answer=='yes' || answer=='y'|| answer=='1'){
+      if (answer == 'yes' || answer == 'y' || answer == '1') {
         resolve(true)
-      }else if (answer=='no' || answer=='n'|| answer=='0'){
+      } else if (answer == 'no' || answer == 'n' || answer == '0') {
         resolve(false)
-      }else{
+      } else {
         reject('Invalid input')
       }
     })
@@ -42,33 +42,40 @@ const playMoreAsk = () => {
 const main = async () => {
   while (cont) {
     // place bet
-    var bet = await placeBet().catch(error => {
-      console.error(error)
-    })
+    var bet = undefined
+    while (typeof bet === 'undefined') {
+      bet = await placeBet().catch(error => {
+        console.error(error)
+      })
+    }
 
     // play
     var result = play()
-    if(result=='win'){
+    if (result == 'win') {
       total_win += bet
       console.log(`You won!!! received ${bet} chips.`)
-    }else if(result=='tie'){
+    } else if (result == 'tie') {
       console.log(`A tie, no chips loss.`)
-    }else if(result=='loss'){
+    } else if (result == 'loss') {
       total_win -= bet
       console.log(`You loss, loss ${bet} chips.`)
     }
 
     // play more ask
-    cont = await playMoreAsk().catch(error => {
-      console.error(error)
-    })
+    var contTmp = undefined
+    while (typeof contTmp === 'undefined') {
+      contTmp = await playMoreAsk().catch(error => {
+        console.error(error)
+      })
+    }
+    cont = contTmp
   }
 
   // show total win
-  if(total_win>=0){
+  if (total_win >= 0) {
     console.log(`You got total ${total_win} chip(s).`)
-  }else{
-    console.log(`You loss total ${total_win*(-1)} chip(s).`)
+  } else {
+    console.log(`You loss total ${total_win * (-1)} chip(s).`)
   }
   rl.close()
 }
